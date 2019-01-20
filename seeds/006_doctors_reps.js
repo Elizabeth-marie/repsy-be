ac52c967bf51fee11c68cb4913ed57dccbbf7af1
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2019-01-20 14:10:17 -0700

    Added seed data to doctors_reps

--- seeds/006_doctors_reps.js
+++ seeds/006_doctors_reps.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('doctors_reps').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('doctors_reps').insert([
+        {id: 1, reps_id: 1, doctors_id: 2},
+        {id: 2, reps_id: 3, doctors_id: 4},
+        {id: 3, reps_id: 1, doctors_id: 3}
+      ])
+      .then(function() { return knex.raw("SELECT setval('doctors_reps_id_seq', (SELECT MAX(id) FROM doctors_reps))")
+      })
+    })
+}
