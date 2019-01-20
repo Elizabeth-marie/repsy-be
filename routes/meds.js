561bf751594543d79d58111a7c1cd79ae8bdf6fe
TaylorArmstrong <TArmstrong1327@gmail.com>
2019-01-20 16:36:22 -0700

    meds routes done

--- routes/meds.js
+++ routes/meds.js
@@ -36,40 +36,42 @@ const validatePostBody = (req, res, next) => {
   next()
 }
 
-// /* Uses joi to build a patch request */
-// const buildPatchReq = (req, res, next) => {
-//   const patchSchema = Joi.object().keys({
-//     fname: Joi.string().required(),
-//     lname: Joi.string().required(),
-//     quiz_points: Joi.integer()
-//   })
-//
-//   const { error } = Joi.validate(req.body, patchSchema)
-//   if (error) {
-//     return res.status(400).json({ "PATCH Schema Error": { message: error.details[0].message } })
-//   }
-//
-//   const allowedPatchKeys = [fname, lname, quiz_points]
-//
-//   // Constructs the patch request object
-//   let patchReq = {}
-//   allowedPatchKeys.forEach(key => {
-//     if (req.body.hasOwnProperty(key)) { patchReq[key] = req.body[key] }
-//   })
-//
-//   // If the patch request is empty or has invalid key names, return an error
-//   if (Object.keys(patchReq).length === 0) {
-//     return res.status(400).json({ error: { message: `Empty or invalid patch request` } })
-//   }
-//
-//   // Every patch update will create a new 'updated_at' timestamp
-//   patchReq.updated_at = new Date()
-//
-//   // Stores the patch request-object into next request
-//   req.patchReq = patchReq
-//   next()
-// }
-//
+/* Uses joi to build a patch request */
+const buildPatchReq = (req, res, next) => {
+  const patchSchema = Joi.object().keys({
+    generic_name: Joi.string().required(),
+    brand_name: Joi.string().required(),
+    pharma_company: Joi.string(),
+    info: Joi.string(),
+    photo: Joi.string(),
+  })
+
+  const { error } = Joi.validate(req.body, patchSchema)
+  if (error) {
+    return res.status(400).json({ "PATCH Schema Error": { message: error.details[0].message } })
+  }
+
+  const allowedPatchKeys = ['generic_name', 'brand_name', 'pharma_company', 'info', 'photo']
+
+  // Constructs the patch request object
+  let patchReq = {}
+  allowedPatchKeys.forEach(key => {
+    if (req.body.hasOwnProperty(key)) { patchReq[key] = req.body[key] }
+  })
+
+  // If the patch request is empty or has invalid key names, return an error
+  if (Object.keys(patchReq).length === 0) {
+    return res.status(400).json({ error: { message: `Empty or invalid patch request` } })
+  }
+
+  // Every patch update will create a new 'updated_at' timestamp
+  patchReq.updated_at = new Date()
+
+  // Stores the patch request-object into next request
+  req.patchReq = patchReq
+  next()
+}
+
 /* GET all users record */
 router.get('/', (req, res, next) => {
   knex('meds').then(data => res.status(200).json(data)).catch(err => next(err))
@@ -87,21 +89,21 @@ router.post('/', validatePostBody, (req, res, next) => {
   knex('meds').insert({ id, generic_name, brand_name, pharma_company, info, photo }).returning('*').then(([data]) => res.status(201).json(data)).catch(err => next(err))
 })
 
-// /* PATCH specified users record */
-// router.patch('/:id', validateUserID, buildPatchReq, (req, res, next) => {
-//   const {patchReq} = req
-//
-//   knex('users').where('id', req.params.id).first().update(patchReq).returning('*').then(([data]) => {
-//     res.status(200).json(data)
-//   }).catch(err => next(err))
-// })
-//
-// /* DELETE specified users record */
-// router.delete('/:id', validateUserID, (req, res, next) => {
-//   knex('users').where('id', req.params.id).first().del().returning('*').then(([data]) => {
-//     console.log('deleted', data)
-//     res.status(200).json({deleted: data})
-//   })
-// })
+/* PATCH specified users record */
+router.patch('/:id', validateUserID, buildPatchReq, (req, res, next) => {
+  const {patchReq} = req
+
+  knex('meds').where('id', req.params.id).first().update(patchReq).returning('*').then(([data]) => {
+    res.status(200).json(data)
+  }).catch(err => next(err))
+})
+
+/* DELETE specified users record */
+router.delete('/:id', validateUserID, (req, res, next) => {
+  knex('meds').where('id', req.params.id).first().del().returning('*').then(([data]) => {
+    console.log('deleted', data)
+    res.status(200).json({deleted: data})
+  })
+})
 
 module.exports = router
