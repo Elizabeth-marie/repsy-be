c136a93bab89d70a3abb0ff11ee013550d63b366
Josh <grafton.josh@gmail.com>
2019-01-20 16:35:22 -0700

    patch route working

--- routes/conditions.js
+++ routes/conditions.js
@@ -33,38 +33,39 @@ const validatePostBody = (req, res, next) => {
 }
 //
 // /* Uses joi to build a patch request */
-// const buildPatchReq = (req, res, next) => {
-//   const patchSchema = Joi.object().keys({
-//     fname: Joi.string().required(),
-//     lname: Joi.string().required(),
-//     quiz_points: Joi.integer()
-//   })
-//
-//   const { error } = Joi.validate(req.body, patchSchema)
-//   if (error) {
-//     return res.status(400).json({ "PATCH Schema Error": { message: error.details[0].message } })
-//   }
-//
-//   const allowedPatchKeys = [fname, lname, quiz_points]
-//
-//   // Constructs the patch request object
-//   let patchReq = {}
-//   allowedPatchKeys.forEach(key => {
-//     if (req.body.hasOwnProperty(key)) { patchReq[key] = req.body[key] }
-//   })
-//
-//   // If the patch request is empty or has invalid key names, return an error
-//   if (Object.keys(patchReq).length === 0) {
-//     return res.status(400).json({ error: { message: `Empty or invalid patch request` } })
-//   }
-//
-//   // Every patch update will create a new 'updated_at' timestamp
-//   patchReq.updated_at = new Date()
-//
-//   // Stores the patch request-object into next request
-//   req.patchReq = patchReq
-//   next()
-// }
+const buildPatchReq = (req, res, next) => {
+  const patchSchema = Joi.object().keys({
+    name: Joi.string().required(),
+    specialties_id: Joi.number().integer().required(),
+  })
+
+  const { error } = Joi.validate(req.body, patchSchema)
+  if (error) {
+    return res.status(400).json({ "PATCH Schema Error": { message: error.details[0].message } })
+  }
+
+  const allowedPatchKeys = ['name', 'specialties_id']
+
+  // Constructs the patch request object
+  let patchReq = {}
+  allowedPatchKeys.forEach(key => {
+    if (req.body.hasOwnProperty(key)) {
+      patchReq[key] = req.body[key]
+    }
+  })
+
+  // If the patch request is empty or has invalid key names, return an error
+  if (Object.keys(patchReq).length === 0) {
+    return res.status(400).json({ error: { message: `Empty or invalid patch request` } })
+  }
+
+  // Every patch update will create a new 'updated_at' timestamp
+  patchReq.updated_at = new Date()
+
+  // Stores the patch request-object into next request
+  req.patchReq = patchReq
+  next()
+}
 //
 /* GET all users record */
 router.get('/', (req, res, next) => {
@@ -85,13 +86,18 @@ router.post('/', validatePostBody, (req, res, next) => {
 })
 //
 // /* PATCH specified users record */
-// router.patch('/:id', validateConditionsID, buildPatchReq, (req, res, next) => {
-//   const {patchReq} = req
-//
-//   knex('users').where('id', req.params.id).first().update(patchReq).returning('*').then(([data]) => {
-//     res.status(200).json(data)
-//   }).catch(err => next(err))
-// })
+router.patch('/:id', validateConditionsID, buildPatchReq, (req, res, next) => {
+  const {patchReq} = req
+
+  knex('conditions')
+  .where('id', req.params.id)
+  .first()
+  .update(patchReq)
+  .returning('*')
+  .then(([data]) => {
+    res.status(200).json(data)
+  }).catch(err => next(err))
+})
 //
 // /* DELETE specified users record */
 // router.delete('/:id', validateConditionsID, (req, res, next) => {
