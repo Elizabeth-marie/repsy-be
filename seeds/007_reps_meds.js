4ab2ab3704ced28dc2b565b24620456ecd9334c8
TaylorArmstrong <TArmstrong1327@gmail.com>
2019-01-20 14:13:19 -0700

    reps_meds done

--- seeds/007_reps_meds.js
+++ seeds/007_reps_meds.js
@@ -1,13 +1,17 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('reps_meds').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('reps_meds').insert([
+        { id: 1, reps_id: 1, meds_id: 1},
+        { id: 2, reps_id: 2, meds_id: 2 },
+        { id: 3, reps_id: 3, meds_id: 3 },
+      ])
+    })
+    .then(function () {
+      //Moves id column (PK) auto-incremented to correct value after inserts
+      return knex.raw(`SELECT setval('reps_meds_id_seq', (SELECT MAX(id) FROM reps_meds))`)
+    })
+}
