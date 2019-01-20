2b893e9d650a909d80b3bc42442181ec5f1895c7
Josh <grafton.josh@gmail.com>
2019-01-20 14:06:31 -0700

    seeds for conditions_meds

--- seeds/008_conditions_meds.js
+++ seeds/008_conditions_meds.js
@@ -1,13 +1,19 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('conditions_meds').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('conditions_meds').insert([
+        {id: 1, meds_id: 1, conditions_id: 1 },
+        {id: 2, meds_id: 2, conditions_id: 2 },
+        {id: 3, meds_id: 3, conditions_id: 3 },
+        {id: 4, meds_id: 2, conditions_id: 1 },
+        {id: 5, meds_id: 3, conditions_id: 1 },
       ]);
     });
+    .then(function () {
+      //Moves id column (PK) auto-incremented to correct value after inserts
+      return knex.raw(`SELECT setval('conditions_meds_id_seq', (SELECT MAX(id) FROM conditions_meds))`)
+    })
 };
