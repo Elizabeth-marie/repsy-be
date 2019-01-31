1f476fc1b7dc6c9951dfa4b02843c9977ab3a292
Josh <grafton.josh@gmail.com>
2019-01-31 15:18:30 -0700

    added join_id column

--- seeds/009_doctors_conditions.js
+++ seeds/009_doctors_conditions.js
@@ -4,18 +4,18 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('doctors_conditions').insert([
-        {id: 1, doctors_id: 1, conditions_id: 1 },
-        {id: 2, doctors_id: 2, conditions_id: 2 },
-        {id: 3, doctors_id: 3, conditions_id: 3 },
-        {id: 4, doctors_id: 2, conditions_id: 1 },
-        {id: 5, doctors_id: 3, conditions_id: 1 },
-        {id: 6, doctors_id: 2, conditions_id: 5 },
-        {id: 7, doctors_id: 2, conditions_id: 17 },
-        {id: 8, doctors_id: 2, conditions_id: 29 },
+        {join_id: 1, doctors_id: 1, conditions_id: 1 },
+        {join_id: 2, doctors_id: 2, conditions_id: 2 },
+        {join_id: 3, doctors_id: 3, conditions_id: 3 },
+        {join_id: 4, doctors_id: 2, conditions_id: 1 },
+        {join_id: 5, doctors_id: 3, conditions_id: 1 },
+        {join_id: 6, doctors_id: 2, conditions_id: 5 },
+        {join_id: 7, doctors_id: 2, conditions_id: 17 },
+        {join_id: 8, doctors_id: 2, conditions_id: 29 },
       ])
     })
     .then(function () {
       //Moves id column (PK) auto-incremented to correct value after inserts
-      return knex.raw(`SELECT setval('doctors_conditions_id_seq', (SELECT MAX(id) FROM doctors_conditions))`)
+      return knex.raw(`SELECT setval('doctors_conditions_join_id_seq', (SELECT MAX(join_id) FROM doctors_conditions))`)
     })
 };
